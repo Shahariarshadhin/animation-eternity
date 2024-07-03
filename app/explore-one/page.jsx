ead88dcd3cc2f2872173f7c05e9625146db8e315
Shahariarshadhin <shadhin19shahriar@gmail.com>
2024-07-03 17:37:48 +0600

    first commit

---
+++ app/explore-one/page.jsx
@@ -0,0 +1,11 @@
+import ExploreOnePublic from "@/components/ExploreOne/ExploreOnePublic"
+
+const page = () => {
+  return (
+    <div>
+        <ExploreOnePublic/>
+    </div>
+  )
+}
+
+export default page
---
+++ app/explore-two/page.jsx
@@ -0,0 +1,11 @@
+import ExploreTwoPublic from "@/components/ExploreTwo/ExploreTwoPublic"
+
+const page = () => {
+  return (
+    <div>
+        <ExploreTwoPublic/>
+    </div>
+  )
+}
+
+export default page
